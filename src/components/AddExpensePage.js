e17a833e52311fca2b0e488cb533081e05bb3a0e
tiqer3k <maciej.majowiec@gmail.com>
2020-02-26 13:21:04 +0100

    test

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -17,7 +17,7 @@ export class AddExpensePage extends React.Component {
             <div>
               <div className="page_header">
                 <div className="content-container">
-                  <h1 className="page-header__title"> Add Expense </h1>
+                  <h2 className="page-header__title"> Add Expense </h2>
                 </div>
               </div>
             <div className="content-container">
