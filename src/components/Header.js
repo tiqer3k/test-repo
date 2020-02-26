b9e55794eddf9bb21919921a604a678f6e845cb1
tiqer3k <maciej.majowiec@gmail.com>
2020-02-26 13:33:28 +0100

    expensify header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export const Header = ({ startLogout }) =>
         <div className="content-container">
         <div className="header__content">
           <Link className="header__title" to="/dashboard">
-            <h1>Expensify</h1>
+            <h2>Expensify</h2>
           </Link>
           <button className="button button--link" onClick={startLogout}>Logout</button>
         </div>
